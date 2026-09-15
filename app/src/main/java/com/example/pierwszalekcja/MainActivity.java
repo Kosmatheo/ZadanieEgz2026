fb57c64c45f58613f52ea93f2b5c1f6fb524e987
Kosmatheo <kosmafzar@gmail.com>
2026-09-15 12:05:41 +0200

    gotowe

--- app/src/main/java/com/example/pierwszalekcja/MainActivity.java
+++ app/src/main/java/com/example/pierwszalekcja/MainActivity.java
@@ -1,6 +1,10 @@
  package com.example.pierwszalekcja;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import org.jspecify.annotations.NullMarked;
+
+ public class MainActivity extends AppCompatActivity {
+    private Button zatwierdz , wlacz;
+    private EditText editText;
+    private TextView NumerPrania , infOOdku;
+    private int licznik = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,39 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        zatwierdz = findViewById(R.id.zatwiedz);
+        wlacz = findViewById(R.id.wlacz);
+        editText = findViewById(R.id.edit);
+        NumerPrania = findViewById(R.id.nrPrania);
+        infOOdku = findViewById(R.id.inf);
+
+        zatwierdz.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        int wartoscPola = Integer.parseInt( editText.getText().toString());
+                        if( wartoscPola >= 1 && wartoscPola <= 12){
+                            NumerPrania.setText("Numer prania: "+wartoscPola);
+                        }
+                    }
+                }
+        );
+        wlacz.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if(licznik % 2 == 0) {
+                            wlacz.setText("Wyłącz");
+                            infOOdku.setText("Odkurzacz włączony");
+                        }
+                        else{
+                            wlacz.setText("Włącz");
+                            infOOdku.setText("Odkurzacz wyłączony");
+                        }
+                        licznik++;
+                    }
+                }
+        );
+
     }
 }
